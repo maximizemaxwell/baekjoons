69687bfce7395730e5f98a1f9e62bdbae50f7f58
MaCAT <138701551+maximizemaxwell@users.noreply.github.com>
2024-04-11 20:11:04 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 284 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	int num;
+	vector <int> vec;
+	for (int i = 0; i < N; i++) {
+		cin >> num;
+		vec.push_back(num);
+	}
+	sort(vec.begin(), vec.end());
+
+	for (int i = 0; i < N; i++) {
+		cout << vec[i] << '\n';
+	}
+}
